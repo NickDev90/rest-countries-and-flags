10842ba7f585c83e69e66365b4c849726c1b842f
NickDev90 <nickie05021990@gmail.com>
2021-10-27 20:59:33 +0200

    requests to API, countries rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import { ALL_COUNTRIES } from './config';
+
 import Controls from './Components/Controls/Controls';
+import List from './Components/Countries/List';
 import Header from './Components/Header';
 import Main from './Components/Main';
+import Card from './Components/Countries/Card';
+
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  console.log(countries);
+
+  useEffect( () => {
+    axios.get(ALL_COUNTRIES)
+    .then( ({data}) => setCountries(data))
+  }, [])
+
+
+
   return (
     <>
     <Header/> 
     <Main>
       <Controls/>
+      <List>
+        {countries.map(c => {
+          const countryInfo = {
+              img: c.flags.png,
+              name: c.name,
+              info: [
+                {
+                  title: 'Population',
+                  description: c.population.toLocaleString()
+                },
+                {
+                  title: 'Region',
+                  description: c.region.toLocaleString()
+                },
+                {
+                  title: 'Capital',
+                  description: c.capital.toLocaleString()
+                },
+              ]
+            }
+          return (
+            <Card key={c.name} {...countryInfo}/>
+          )
+        }
+        
+        )}
+      </List>
     </Main>
     </>
   );
---
+++ src/config.js
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://restcountries.com/v2/';
+
+export const ALL_COUNTRIES = BASE_URL + 'all?fields=name,capital,flags,population,region,borders';
+
+export const searchByCoutry = (name) => BASE_URL + 'name/' + name;
+
+export const filterByCodes = (codes) => BASE_URL + 'alpha?codes=' + codes.join(',');
+
